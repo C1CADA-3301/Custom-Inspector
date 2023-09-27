24f2415d1cf84858251a7103df532514a950916a
Vivek R Menon <61944839+C1CADA-3301@users.noreply.github.com>
2023-09-27 21:02:16 +0530

    Update UItool.cs

--- Assets/Editor/UItool.cs
+++ Assets/Editor/UItool.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 using UnityEditor.PackageManager.UI;
+using UnityEngine.UI;
+using TMPro;
 
 public class UItool : EditorWindow
 {
@@ -11,6 +13,10 @@ public class UItool : EditorWindow
     GameObject cube;
     TextAsset jsonFile;
     Vector2 scroll;
+    GameObject globalParentObject;
+
+    List<GameObject> generatedObjects = new List<GameObject>();
+
     string uiParentName = "God Father";
 
     [MenuItem("Tools/Basic UI Setup")] //Tool ddress
@@ -33,12 +39,14 @@ public class UItool : EditorWindow
         uiParentName = EditorGUILayout.TextField("UI Parent Name", uiParentName); //Scroll View
         jsonFile = EditorGUILayout.ObjectField("JSON File", jsonFile, typeof(TextAsset), false) as TextAsset; //Json File
         cube = EditorGUILayout.ObjectField("Game Object", cube, typeof(GameObject), false) as GameObject; //Object To Spawn
+        globalParentObject = EditorGUILayout.ObjectField("Parent Object", Selection.activeGameObject, typeof(GameObject), false) as GameObject; 
 
         scroll =  GUILayout.BeginScrollView(scroll); //#Scroll Start
 
         //----------------PROPERTIES-------------------#end
         
         ShowJsonArrayGUI(); //Show the JSON Data in an array format in Editor Window.
+       
 
         GUILayout.EndScrollView(); //#Scroll end
 
@@ -46,20 +54,19 @@ public class UItool : EditorWindow
 
         if (GUILayout.Button("Read Json")) //Reads the JSON File.
         {
-
-            myList = jsonFile != null ? JsonUtility.FromJson<ObjectList>(jsonFile.text) : null;
+            ReadJSON();
+            
         }
         
 
         if (GUILayout.Button("Save Json"))  //Saves the JSON File.
         {
-            string json = JsonUtility.ToJson(myList);
-            File.WriteAllText(AssetDatabase.GetAssetPath(jsonFile),json);
+            SaveJSON();
         }
 
         if (GUILayout.Button("clear Json")) //Clears the JSON File.
         {
-            myList = null;
+            ClearJSON();
         }
 
         if (GUILayout.Button("Generate UI")) //Generates the UI Object.
@@ -67,9 +74,14 @@ public class UItool : EditorWindow
             GenerateObject();
         }
 
+        if(GUILayout.Button("Update generated object Data")) //Updates generated object data
+        {
+            UpdateObjectData();
+        }
+
         if(GUILayout.Button("Destroy OBJ")) //Destroys the selected Object.
         {
-            DestroyParent(uiParentName);
+            DestroyParent(globalParentObject.name);
         }
 
         //----------------BUTTONS-------------------#end
@@ -85,50 +97,146 @@ public class UItool : EditorWindow
         so.ApplyModifiedProperties();
     }
 
+    void ReadJSON()
+    {
+        myList = jsonFile != null ? JsonUtility.FromJson<ObjectList>(jsonFile.text) : null;
+    }
+    void SaveJSON()
+    {
+        string json = JsonUtility.ToJson(myList);
+        File.WriteAllText(AssetDatabase.GetAssetPath(jsonFile), json);
+        Debug.Log("Saved JSON");
+    }
+
+    void ClearJSON()
+    {
+        myList = null;
+    }
     void GenerateObject() //Function to Generata Object.
     {
-        
-        if (cube == null) return;
-       
-        GameObject GodFather = new GameObject();
-        GodFather.name = uiParentName;
-        ObjectData[] objList = myList.StartUI; 
+        generatedObjects.Clear();
 
+        if (globalParentObject == null)
+        {
+            globalParentObject = new GameObject();
+            globalParentObject.name = "Basic UI Setup";
+        }
+
+        ObjectData[] objList = myList.StartUI; 
         for(int i=0;i<objList.Length;i++) // This loop Itertes though all the elements in the ObjList Array.
         {
-           
-            string objName = objList[i].name;
-            Vector3 objPos = new Vector3(objList[i].posx, objList[i].posy, objList[i].posz);
-            Vector3 objSize = new Vector3(objList[i].size, objList[i].size, objList[i].size);
 
-            GameObject obj = Instantiate(cube,objPos,  Quaternion.identity,GodFather.transform);
-            obj.transform.localScale = objSize; //sets Size of the new object.
-
-            if (objList[i].parent!=null) //Sets the parent
-            {
-               GameObject parent =  SearchParent(GodFather, objList[i].parent);
-                if(parent != null) 
-                {
-                    if (parent.name != objList[i].parent)
-                    {
-                        parent = GodFather;
-                    }
-                    obj.transform.SetParent(parent.transform);
-                    
-                }
-
-            }
-
-            obj.name = objName; //sets Position of the new object.
-            obj.transform.position = objPos; //sets Name of the new object.
-            
+            GenerateObjectOfType(objList[i],objList[i].uiType, SearchParent(globalParentObject, objList[i].parent));
 
         }
     }
+
+    void GenerateObjectOfType(ObjectData obj, UIType uitype, GameObject parent)
+    {
+        if (uitype == UIType.Canvas)
+        {
+            GameObject canvas = new GameObject();
+            canvas.AddComponent<Canvas>();
+            canvas.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            canvas.AddComponent<CanvasScaler>();
+            canvas.AddComponent<GraphicRaycaster>();
+            canvas.gameObject.name = obj.name;
+            canvas.transform.SetParent(parent.transform);
+            generatedObjects.Add(canvas);
+
+
+        }
+
+        if (uitype == UIType.GameObject)
+        {
+            GameObject newObject = new GameObject();
+            newObject.name = obj.name;
+            newObject.transform.SetParent(parent.transform);
+            generatedObjects.Add(newObject);
+        }
+
+        if (uitype == UIType.Image)
+        {
+            Image image = new GameObject().AddComponent<Image>();
+            image.gameObject.name = obj.name;
+            image.gameObject.transform.SetParent(parent.transform);
+            image.gameObject.GetComponent<RectTransform>().anchorMin = obj.anchorMin;
+            image.gameObject.GetComponent<RectTransform>().anchorMax = obj.anchorMax;
+            image.gameObject.GetComponent<RectTransform>().sizeDelta = obj.sizeDelta;
+            image.gameObject.GetComponent<RectTransform>().anchoredPosition = obj.anchoredPosition;
+
+            generatedObjects.Add(image.gameObject);
+        }
+
+        if (uitype == UIType.Panel)
+        {
+            Image image = new GameObject().AddComponent<Image>();
+            image.gameObject.name = obj.name;
+            image.gameObject.transform.SetParent(parent.transform);
+            image.gameObject.GetComponent<RectTransform>().anchorMin = obj.anchorMin;
+            image.gameObject.GetComponent<RectTransform>().anchorMax = obj.anchorMax;
+            image.gameObject.GetComponent<RectTransform>().sizeDelta = obj.sizeDelta;
+            image.gameObject.GetComponent<RectTransform>().anchoredPosition = obj.anchoredPosition;
+            image.color = obj.color;
+            // image.rectTransform.sizeDelta = new Vector2(0, 0);
+            generatedObjects.Add(image.gameObject);
+
+        }
+
+        if (uitype == UIType.Text)
+        {
+            TextMeshProUGUI text = new GameObject().AddComponent<TextMeshProUGUI>();
+            text.gameObject.gameObject.name = obj.name;
+            text.gameObject.gameObject.transform.SetParent(parent.transform);
+
+            text.gameObject.GetComponent<RectTransform>().anchorMin = obj.anchorMin;
+            text.gameObject.GetComponent<RectTransform>().anchorMax = obj.anchorMax;
+            text.gameObject.GetComponent<RectTransform>().sizeDelta = obj.sizeDelta;
+            text.gameObject.GetComponent<RectTransform>().anchoredPosition = obj.anchoredPosition;
+            text.color = obj.color;
+
+            generatedObjects.Add(text.gameObject);
+        }
+        if (uitype == UIType.Button)
+        {
+            GameObject button = new GameObject();
+            button.AddComponent<CanvasRenderer>();
+            button.AddComponent<Image>();
+            button.AddComponent<Button>();
+            button.gameObject.name = obj.name;
+
+            button.gameObject.transform.SetParent(parent.transform);
+            button.gameObject.GetComponent<RectTransform>().anchorMin = obj.anchorMin;
+            button.gameObject.GetComponent<RectTransform>().anchorMax = obj.anchorMax;
+
+            button.gameObject.GetComponent<RectTransform>().anchoredPosition = obj.anchoredPosition;
+            button.gameObject.GetComponent<RectTransform>().sizeDelta = obj.sizeDelta;
+
+            Color normalCol = button.GetComponent<Button>().colors.normalColor;
+            normalCol = obj.color;
+
+            // ---------------------------BUTTON TEXT-----------------------------------#start
+
+
+            GameObject buttonText = new GameObject();
+            buttonText.AddComponent<TextMeshProUGUI>();
+            buttonText.gameObject.AddComponent<CanvasRenderer>();
+            buttonText.name = obj.name + "_ButtonText";
+            buttonText.GetComponent<TextMeshProUGUI>().text = obj.name + "_Button";
+            buttonText.GetComponent<TextMeshProUGUI>().color = Color.black;
+            buttonText.transform.SetParent(button.transform);
+            buttonText.gameObject.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+
+            // ---------------------------BUTTON TEXT-----------------------------------#end
+            generatedObjects.Add(button);
+        }
+
+    }
+
     GameObject foundparent;
     GameObject SearchParent(GameObject parentObj,string parentName)  //Function to Search the object to set as parent.
     {
-        
+        GameObject godparent = parentObj;
         foreach(Transform t in parentObj.transform)
         {
             if(t.name==parentName)
@@ -140,6 +248,12 @@ public class UItool : EditorWindow
                 SearchParent(t.gameObject, parentName);
             }
         }
+        if(foundparent==null || foundparent.name !=parentName)
+        {
+            foundparent = parentObj;
+        }
+
+      
         
         return foundparent;
     }
@@ -155,16 +269,43 @@ public class UItool : EditorWindow
             }
         }
     }
+
+   
+
+    void UpdateObjectData()
+    {
+        ObjectData[] objList = myList.StartUI;
+        for(int i=0;i<objList.Length;i++)
+        {
+            objList[i].anchoredPosition = generatedObjects[i].GetComponent<RectTransform>()? generatedObjects[i].GetComponent<RectTransform>().anchoredPosition:Vector2.zero;
+            objList[i].anchorMin = generatedObjects[i].GetComponent<RectTransform>()? generatedObjects[i].GetComponent<RectTransform>().anchorMax:Vector2.zero;
+            objList[i].anchorMin = generatedObjects[i].GetComponent<RectTransform>()? generatedObjects[i].GetComponent<RectTransform>().anchorMin:Vector2.zero;
+            objList[i].sizeDelta = generatedObjects[i].GetComponent<RectTransform>()? generatedObjects[i].GetComponent<RectTransform>().sizeDelta:Vector2.zero;
+            //objList[i].col = generatedObjects[i].GetComponent<RectTransform>()? generatedObjects[i].GetComponent<RectTransform>().sizeDelta:Vector2.zero;
+            Debug.Log("UPDATED "+ generatedObjects[i]);
+           
+        }
+        SaveJSON();
+        ClearJSON();
+        ReadJSON();
+       
+        
+       
+    }
 }
 [System.Serializable]
 public class ObjectData
 {
     public string name;
     public string parent;
-    public int posx;
-    public int posy;
-    public int posz;
-    public int size;
+    public UIType uiType;
+    public Vector3 Position;
+    public Vector2 anchorMin;
+    public Vector2 anchorMax;
+    public Vector3 anchoredPosition;
+    public Vector3 sizeDelta;
+    public Color color = Color.white;
+    
 }
 
 [System.Serializable]
@@ -173,5 +314,14 @@ public class ObjectList
     public ObjectData[] StartUI;
 }
 
+public enum UIType
+{
+    GameObject,
+    Canvas,
+    Panel,
+    Image,
+    Text,
+    Button
+}
 
 
